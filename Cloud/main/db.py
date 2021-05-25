10e8433f3723b86394ce27767192179a8441c289
Denny Lee <71874926+dennylee22@users.noreply.github.com>
2021-05-25 13:30:29 +0700

    Updating db.py, adding 1 more function for list feature

--- Cloud/main/db.py
+++ Cloud/main/db.py
@@ -13,3 +13,13 @@ def get_solusi(penyakit):
 	for x in range(0,len(rows)):
 		solusi.append(rows[x][1])
 	return(solusi)
+
+def list_penyakit():
+	l = []
+	con = mysql.connect(host=host, database=db, user=user, password=password)
+	cursor = con.cursor()
+	cursor.execute("SELECT DISTINCT nama FROM solusi")
+	rows = cursor.fetchall()
+	for x in range(0,len(rows)):
+		l.append(rows[x][0])
+	return(l)

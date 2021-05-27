2f444a9ca67bd95b5961a960e574902006a780aa
Denny Lee <71874926+dennylee22@users.noreply.github.com>
2021-05-27 21:42:32 +0700

    Making the API RESTful with flask_restful

--- Cloud/main/main.py
+++ Cloud/main/main.py
@@ -10,10 +10,12 @@ import json
 import requests
 import os
 from savedata import save_data
+from flask_restful import Api, Resource
 
 model_url = 'http://192.168.1.4:9000/model'
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+api = Api(app)
 
 dict = {0:'Hawar Daun Bakteri/Kresek',1:'Bercak Daun Cokelat',2:'Leaf Smut'}
 allowed_files = {'.png','.jpg','.jpeg'}
@@ -22,9 +24,8 @@ allowed_mime = {'image/png','image/jpeg','image/jpg'}
 #API untuk upload gambar dari user
 #URL buat endpoint http://34.126.165.65:9000/upload
 #Bikin HTTP POST request ke URL-nya, pake variabel "file" buat gambarnya
-@app.route('/upload', methods=['POST'])
-def upload():
-	if request.method == "POST" or request.method == "post":
+class Upload(Resource):
+	def post(self):
 		if "file" in request.files:
 			file = request.files['file']
 			if pathlib.Path(file.filename).suffix in allowed_files :
@@ -34,7 +35,8 @@ def upload():
 					files = {'file': (filename, open('/images/'+filename, 'rb'), file.mimetype)}
 					os.remove('/images/' + filename)
 					r = requests.post(model_url,files=files)
-					penyakit = r.text
+					penyakit = r.text[1:-2]
+					#print(penyakit[1:-2])
 					solusi = get_solusi(penyakit)
 					result = {"penyakit":penyakit,"solusi":solusi}
 					#Masukkin data ke BigQuery
@@ -42,30 +44,30 @@ def upload():
 					lokasi=None
 					if penyakit != "tidak ada penyakit yang terdeteksi":
 						feedback = save_data(penyakit, lokasi)
-					return json.dumps(result)
-				#expected return:
-				#{"penyakit": "Leaf Smut", "solusi": ["Menjaga kebersihan sawah dari sisa - sisa tanaman yang terinfeksi", "hindari penggunaan pupuk Nitrogen yang berlebihan", "Jika masih terinfeksi, tanam varietas padi yang memiliki ketahanan terhadap leaf smut."]}
+					return(result)
+					#expected return:
+					#{"penyakit": "Leaf Smut", "solusi": ["Menjaga kebersihan sawah dari sisa - sisa tanaman yang terinfeksi", "hindari penggunaan pupuk Nitrogen yang berlebihan", "Jika masih terinfeksi, tanam varietas padi yang memiliki ketahanan terhadap leaf smut."]}
 				else:
 					error = "Mimetype not allowed"
 			else:
 				error = "File is not an image"
 		else:
 			error = "File not found"
-	else:
-		error = "Method not allowed"
-	if error:
-		return(json.dumps({"error" : error}))
+		if error:
+			return({"error" : error})
+
+
+api.add_resource(Upload,"/upload")
 
 #API untuk fitur list semua penyakit
 #URL buat endpoint http://34.126.165.65:9000/list ato http://34.126.165.65:9000/list?id=1
 #bisa list solusi dari sebuah penyakit lewat request GET dan kasih id dari penyakit
-@app.route('/list', methods=['GET'])
-def list():
-	if request.method == "GET" or request.method == "get":
+class List(Resource):
+	def get(self):
 		if request.args.get("id"):
 			id = request.args.get("id", type = int)
 			res = get_solusi(dict[id])
-			return jsonify({"penyakit":dict[id],"solusi":res})
+			return({"penyakit":dict[id],"solusi":res})
 			#expected return:
 			#{
 			#  "penyakit": "Leaf Smut", 
@@ -77,7 +79,7 @@ def list():
 			#}
 		else:
 			res = list_penyakit()
-			return jsonify({"penyakit":res})
+			return({"penyakit":res})
 			#expected return
 			#{
 			#  "penyakit": [
@@ -86,11 +88,6 @@ def list():
 			#    "Leaf Smut"
 			#  ]
 			#}
-
-	else:
-		error = "Method not allowed"
-
-	if error:
-		return(json.dumps({"error" : error}))
+api.add_resource(List,"/list")
 
 app.run(host='0.0.0.0',port=9000)

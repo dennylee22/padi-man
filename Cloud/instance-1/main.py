140b23d0a2063cc4fd90463d81c9dd8895a299ea
Denny Lee <71874926+dennylee22@users.noreply.github.com>
2021-05-27 21:43:23 +0700

    making the API RESTful with flask_restful

--- Cloud/instance-1/main.py
+++ Cloud/instance-1/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import flask
 import mimetypes
 from flask import request, jsonify
@@ -5,20 +6,22 @@ import pathlib
 from werkzeug.utils import secure_filename
 from ml import get_penyakit
 import os
+from flask_restful import Api, Resource
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+api = Api(app)
+daftar_penyakit = {-1:'tidak ada penyakit yang terdeteksi', 0:'Hawar Daun Bakteri/Kresek', 1:'Bercak Daun Cokelat', 2:'Leaf Smut'}
 
-dict = {0:'Hawar Daun Bakteri/Kresek',1:'Bercak Daun Cokelat',2:'Leaf Smut'}
-
-
-@app.route('/model', methods=['POST'])
-def model():
-	file = request.files['file']
-	filename = secure_filename(file.filename)
-	file.save("/images/" + filename)
-	res = dict[get_penyakit("/images/"+filename)]
-	os.remove("/images/" + filename)
-	return (res)
+class Model(Resource):
+	def post(self):
+		file = request.files['file']
+		filename = secure_filename(file.filename)
+		file.save("/images/" + filename)
+		res = daftar_penyakit[get_penyakit("/images/"+filename)]
+		os.remove("/images/" + filename)
+		#print(res)
+		return(res)
+api.add_resource(Model,"/model")
 
 app.run(host='0.0.0.0',port=9000)
